0e7d0f6f8e2b45ad61214920a18a675a6953a4a7
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-16 16:16:50 +0000

    Add create and update routes

--- Week_3/Day_2/movie-quotes/server.js
+++ Week_3/Day_2/movie-quotes/server.js
@@ -56,45 +56,94 @@ const quoteComments = {
   },
 };
 
-// HELPER FUNCTIONS
+// HELPER FUNCTIONS ////////////////////////////
 
+const addNewQuote = content => {
+  // Generate a new id
+  const quoteId = Math.random().toString(26).substring(2, 8);
+  // Create a new quote
+  const newQuote = {
+    id: quoteId,
+    quote: content
+  };
+  // Add the quote to movieQuotesDb
+  movieQuotesDb[quoteId] = newQuote;
+  // Return the quote id
+  return quoteId;
+};
 
+const updateQuote = (quoteId, content) => {
+  // Update the content of the quote with the given id
+  movieQuotesDb[quoteId].quote = content;
+};
 
-// END POINTS OR ROUTES
+// END POINTS OR ROUTES ////////////////////////////
 
 app.get("/", (req, res) => {
   res.send("Welcome to the movie quotes app!");
 });
 
+app.get('/quotes.json', (req, res) => {
+  res.send(movieQuotesDb);
+});
 
-// CRUD Operations on quotes
+// CRUD OPERATIONS ////////////////////////////
 
 // READ: Get the list of all quotes
 app.get("/quotes", (req, res) => {
   // req => request object containing all the info about the request
   // res => response object containing info about the response (also methods)
-
-  // Get an array of quotes from the db
   const quoteList = Object.values(movieQuotesDb);
-
-  const templateVars = { quoteList }
-
-  // Render a page and pass it the object
+  const templateVars = { quoteList };
+  // Render a page and pass in an object containing all quotes from the database
   res.render("display_quotes", templateVars);
 });
 
 // READ: Display only one quote
+app.get("/quotes/:quoteId", (req, res) => {
+  // Extract the quote id from the path
+  const quoteID = req.params.quoteId;
+  const templateVars = { quote: movieQuotesDb[quoteID] };
+  // Render a show page for the given ID
+  res.render("quote_show", templateVars);
+});
 
 // CREATE: Create a new quote
 // a) Display the new form
 // b) Add the new quote to the database
+app.post('/quotes', (req, res) => {
+  // Extract the quote info from the post request
+  const quoteContent = req.body.quoteContent;  // quoteContent is the "name" attribute of the form
+  // Create a new quote in the database
+  const quoteId = addNewQuote(quoteContent);
+  // Redirect to the show page
+  res.redirect(`/quotes/${quoteId}`);
+});
 
 // UPDATE: Update a quote
 // a) Display the update form
 // b) Update the quote in the database
 
+app.post("/quotes/:quoteId", (req, res) => {
+  // Extract the quote id from the path
+  const quoteId = req.params.quoteId;
+  // Extract the content from the form
+  const quoteContent = req.body.quoteContent; // quoteContent is the "name" attribute of the form
+  // Update the quote in the database
+  updateQuote(quoteId, quoteContent);
+  // Redirect to /quotes
+  res.redirect("/quotes");
+});
+
 // DELETE: Delete a quote
 // Delete a quote from the database
+
+app.post("/quotes/:quoteId/delete", (req, res) => {
+  // Delete the quote from the db
+  delete movieQuotesDb[req.params.quoteId];
+  // Redirect to main page
+  res.redirect("/quotes");
+});
 
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
